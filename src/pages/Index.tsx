ea5ffe376b0c2f1ac2cde40d48709ce91680f999
Norman Pettus <nmpettus@yahoo.com>
2025-05-16 07:23:20 -0400

    Fix Index page component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import Navigation from "@/components/Navigation";
+import Hero from "@/components/sections/Hero";
 import Books from "@/components/sections/Books";
 import MeetMaggie from "@/components/sections/MeetMaggie";
 import Activities from "@/components/sections/Activities";
@@ -7,4 +10,27 @@ import Contact from "@/components/sections/Contact";
 import Footer from "@/components/sections/Footer";
 import BackToTopButton from "@/components/BackToTopButton";
 
-export default BackToTopButton
+const Index = () => {
+  return (
+    <div className="font-sans min-h-screen">
+      <Navigation />
+      <Hero />
+      <div id="books">
+        <Books />
+      </div>
+      <MeetMaggie />
+      <div id="activities">
+        <Activities />
+      </div>
+      <FutureProjects />
+      <div id="newsletter">
+        <Newsletter />
+      </div>
+      <Contact />
+      <Footer />
+      <BackToTopButton />
+    </div>
+  );
+};
+
+export default Index;

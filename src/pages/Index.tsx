d59744015ec5cb29e33374ddbdd5966ce5939775
Norman Pettus <nmpettus@yahoo.com>
2025-05-16 07:21:14 -0400

    Updated package-lock.json

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
-@@ .. @@
- import Books from "@/components/sections/Books";
- import MeetMaggie from "@/components/sections/MeetMaggie";
- import Activities from "@/components/sections/Activities";
- import FutureProjects from "@/components/sections/FutureProjects";
--import Newsletter from "@/components/sections/Newsletter";
-+import Newsletter from "@/components/sections/NewsLetter.tsx";
- import Contact from "@/components/sections/Contact";
- import Footer from "@/components/sections/Footer";
- import BackToTopButton from "@/components/BackToTopButton";
+import Books from "@/components/sections/Books";
+import MeetMaggie from "@/components/sections/MeetMaggie";
+import Activities from "@/components/sections/Activities";
+import FutureProjects from "@/components/sections/FutureProjects";
+import Newsletter from "@/components/sections/NewsLetter.tsx";
+import Contact from "@/components/sections/Contact";
+import Footer from "@/components/sections/Footer";
+import BackToTopButton from "@/components/BackToTopButton";
+
+export default BackToTopButton
 
 export default BackToTopButton

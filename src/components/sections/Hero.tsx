2ff7a9aeda7c26f1dca44980c8dfc4651f6c5a66
Norman Pettus <nmpettus@yahoo.com>
2025-05-16 07:54:46 -0400

    Restore original image paths

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
           </div>
           <div className="flex justify-center relative">
             <img 
-              src="https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg" 
+              src="/lovable-uploads/22798029-d558-453e-8673-fa3d5ec62840.png" 
               alt="Maggie with books" 
               className="rounded-xl shadow-2xl max-w-full h-auto" 
             />
